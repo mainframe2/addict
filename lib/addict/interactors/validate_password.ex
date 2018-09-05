b2073c312d255ef39d294b4c48eae29750a6845e
Matija Milcic <ze@vibe.rs>
2018-09-05 14:02:10 +0200

    Add format_response for reset password validation and change error messages content

--- lib/addict/interactors/validate_password.ex
+++ lib/addict/interactors/validate_password.ex
@@ -5,6 +5,14 @@ defmodule Addict.Interactors.ValidatePassword do
 
   Returns `{:ok, []}` or `{:error, [errors]}`
   """
+
+  @password_length            8
+  @uppercase_condition_regex ~r/[A-Z]+/
+  @lowercase_condition_regex ~r/[a-z]+/
+  @number_condition_regex    ~r/\d+/
+  @special_characters_regex  ~r/[.,!?@#$%^&*()<>]+/
+  @password_regex_validations [@uppercase_condition_regex, @lowercase_condition_regex, @number_condition_regex, @special_characters_regex]
+
   def call(changeset, nil) do
     call(changeset, [])
   end
@@ -31,24 +39,8 @@ defmodule Addict.Interactors.ValidatePassword do
     {:error, messages}
   end
 
-  defp validate(:default, password) when is_bitstring(password) do
-    if String.length(password) > 5, do: [], else: [{:password, {"is too short", []}}]
-  end
-
-  defp validate(:default, changeset) do
-    Ecto.Changeset.validate_change(changeset, :password, fn (_field, value) ->
-      validate(:default, value)
-    end).errors
-  end
-
-  @uppercase_condition_regex ~r/[A-Z]+/
-  @lowercase_condition_regex ~r/[a-z]+/
-  @number_condition_regex    ~r/\d+/
-  @special_characters_regex  ~r/[.,!?@#$%^&*()<>]+/
-  @password_regex_validations [@uppercase_condition_regex, @lowercase_condition_regex, @number_condition_regex, @special_characters_regex]
-
   defp validate(:frame_password, :length, password) when is_bitstring(password) do
-    if String.length(password) > 7, do: [], else: [{:password, {"is too short", []}}]
+    if String.length(password) > @password_length - 1, do: [], else: [{:password, {"must be at least #{@password_length} characters long", []}}]
   end
 
   defp validate(:frame_password, :uppercase, password) when is_bitstring(password) do
@@ -65,6 +57,16 @@ defmodule Addict.Interactors.ValidatePassword do
 
   defp validate(:frame_password, :special_chars, password) when is_bitstring(password) do
     if Regex.match?(@special_characters_regex, password), do: [], else: [{:password, {"must contain at least one special character", []}}]
+  end
+
+  defp validate(:default, password) when is_bitstring(password) do
+    if String.length(password) > 5, do: [], else: [{:password, {"is too short", []}}]
+  end
+
+  defp validate(:default, changeset) do
+    Ecto.Changeset.validate_change(changeset, :password, fn (_field, value) ->
+      validate(:default, value)
+    end).errors
   end
 
   defp validate(:frame_password, changeset) do
@@ -84,8 +86,8 @@ defmodule Addict.Interactors.ValidatePassword do
     Enum.map(@password_regex_validations, fn regex -> Regex.match?(regex, password) end)
     |> Enum.count(fn valid? -> valid? end)
     |> case do
-        num when num < 3 -> [{:password, {"password incomplex", []}}]
-        num              -> []
+        num when num < 3 -> [{:password, {"must contain at least one uppercase, lowercase letter, number and special character", []}}]
+        _                -> []
       end
   end
 
