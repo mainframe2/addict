ed90d31dcd2da83f54036dd3813d0f8458e61738
Nikola Mladenovic <nikola.mladenovic@nutanix.com>
2018-11-22 13:00:09 -0800

    Small fix

--- lib/addict/mailers/mail_sender.ex
+++ lib/addict/mailers/mail_sender.ex
@@ -20,6 +20,7 @@ defmodule Addict.Mailers.MailSender do
 
   def send_reset_token(email, path, host \\ Addict.Configs.host) do
     host = host || "http://localhost:4000"
+    template =
     case Addict.Configs.email_reset_password_template do
       nil -> "<p>You've requested to reset your password. Click <a href='#{host}<%= path %>'>here</a> to proceed!</p>"
       {module, method} -> apply(module, method, [email, path, host])
