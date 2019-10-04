3f9814ad624844b82e952e6f28004d7b04ab7ff2
Nemanja Stanarevic <nemanja.stanarevic@nutanix.com>
2019-10-04 14:21:22 -0700

    Removing Ecto adapter from the config.

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :phoenix, :json_library, Jason
 
 config :addict, TestAddictRepo,
-  adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
   database: "addict_test",
